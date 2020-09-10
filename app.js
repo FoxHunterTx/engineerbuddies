1a855bfbd2bd0a1e292200568e3508351ca5008e
FoxHunterTx <foxhuntintx@gmail.com>
2020-09-10 17:49:41 -0500

    with ssl

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const fs = require('fs');
+const https = require('https');
 
 const errorController = require('./controller/error_controller');
 
 const app = express();
+
+const privateKey = fs.readFileSync('engineerbuddies.key');
+const publicKey = fs.readFileSync('www_engineerbuddies_com.crt');
 
 //set view engine to EJS - dynamic pages
 app.set('view engine','ejs');
@@ -26,4 +31,5 @@ app.use(publicRoutes);
 // no fitting of any router path ->catch all error 
 app.use(errorController.get404error); 
 
-app.listen(3000);
+// app.listen(3000);
+https.createServer( {key: privateKey, cert: publicKey},app).listen(8443);
